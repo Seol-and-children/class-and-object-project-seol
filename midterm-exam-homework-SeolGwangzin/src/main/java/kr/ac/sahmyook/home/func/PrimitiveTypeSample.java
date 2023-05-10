a104cca02a37852d52086fa81d1fb176c138d8fd
SeolGwangzin <87467029+SeolGwangzin@users.noreply.github.com>
2023-05-10 15:31:54 +0900

    Delete midterm-exam-homework-SeolGwangzin directory

--- midterm-exam-homework-SeolGwangzin/src/main/java/kr/ac/sahmyook/home/func/PrimitiveTypeSample.java
+++
@@ -1,7 +0,0 @@
-package kr.ac.sahmyook.home.func;
-
-public class PrimitiveTypeSample {
-    public void typeSize(){
-        System.out.println("char : 1byte\nint : 4byte\nfloat : 4byte, \ndouble : 8byte)\nlong double : 8~16byte");
-    }
-}
